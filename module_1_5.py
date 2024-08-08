2e9f6357981b8c0fd7420af9ab861682734fe326
saa1986 <1986zip@mail.ru>
2024-08-08 21:07:23 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+tuple_immutable_var = 1, 2, 3, True
+print(tuple_immutable_var) # кортеж – это неизменяемая упорядоченная коллекция,которая может содержать в себе разные типы данных
+tuple_mutable_list = ([4, 5], 6)
+print(tuple_mutable_list)
+tuple_mutable_list[0][1] = 7
+print(tuple_mutable_list)
